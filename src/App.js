80b22ab4151cd0e1263bbede813e4ad9e238e380
RichardChileya <richardchileyawk@gmail.com>
2023-04-14 12:16:47 +0200

    ADD App.js basic structure

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <>
+      <div id="main-container">
+        <Nav />
+
+        <div className="page-hero">
+          <h1 id="main-heading">
+            I am Richard, Glad to see you!
+          </h1>
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
