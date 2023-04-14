7957ceb3dfdab56810b3364bfcd78158b38877f6
RichardChileya <richardchileyawk@gmail.com>
2023-04-14 12:17:25 +0200

    Add Nav.js component Basic Structure

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="logo-name">
+        <h2 id="name">RIchard</h2>
+      </div>
+      <ul className="nav-ul">
+        <li className="nav-li">Portfolio</li>
+        <li className="nav-li">About</li>
+        <li className="nav-li">Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
